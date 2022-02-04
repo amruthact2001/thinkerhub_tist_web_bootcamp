8d2a246a645485ea8907dcc3ab26f2b9d7ebe0d7
amruthact2001 <84323185+amruthact2001@users.noreply.github.com>
2022-02-04 17:22:40 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,12 @@
+function changeToYellow() {
+    document.body.style.backgroundColor = "Yellow";
+  }
+  function changeToGreen() {
+    document.body.style.backgroundColor = "green";
+  }
+  function changeToRed() {
+    document.body.style.backgroundColor = "red";
+  }
+  function changeToBlack() {
+    document.body.style.backgroundColor = "black";
+  }
